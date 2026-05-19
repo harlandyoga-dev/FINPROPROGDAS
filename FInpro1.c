8f39312da8f83ddcfe132186ad23e75b33ebd6c5
harlandyoga <harlandyoga@gmail.com>
2026-05-19 21:45:09 +0700

    harland 1, hitungalokasi ahhh

--- FInpro1.c
+++ FInpro1.c
@@ -1,2 +1,9 @@
 #include <stdio.h>
 
+void hitungAlokasi(float* pendapatan, Alokasi* alokasi, int bulan, int dewasa, int anak){
+    for(int i = 0; i < bulan; i++){
+        alokasi[i].utama = pendapatan[i] * 1 / (1 + 0.5 * (dewasa - 1) + 0.3 * anak);
+        alokasi[i].dewasa = pendapatan[i] * 0.5 / (1 + 0.5 * (dewasa - 1) + 0.3 * anak);
+        alokasi[i].anak = pendapatan[i] * 0.3 / (1 + 0.5 * (dewasa - 1) + 0.3 * anak);
+    }
+}
